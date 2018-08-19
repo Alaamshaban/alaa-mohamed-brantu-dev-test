5859539cbb308d8500784ce886470b3575464ad0
alaashaban <alaamohamedshaban@yahoo.com>
2018-08-19 09:25:16 -0700

    --

--- client/src/containers/product_list.js
+++ client/src/containers/product_list.js
@@ -12,20 +12,22 @@ class productsList extends Component {
   }
 
   renderProducts() {
-    if (this.props.productsList.length == 0) {
-      return (
-        <div className="no-products">No results found!!</div>
-      )
+    if (this.props.productsList) {
+      if (this.props.productsList.length == 0) {
+        return (
+          <div className="no-products">No results found!!</div>
+        )
+      }
+      return this.props.productsList.map(product => {
+        return (
+          <li className="list-group-item" key={product._id}>
+            <strong className="col-md-4 pull-xs-right">{product.category.name}</strong>
+            <span className="col-md-8">{product.name}</span>
+            <img src={product.image} />
+          </li>
+        );
+      });
     }
-    return this.props.productsList.map(product => {
-      return (
-        <li className="list-group-item" key={product._id}>
-          <strong className="col-md-4 pull-xs-right">{product.category.name}</strong>
-          <span className="col-md-8">{product.name}</span>
-          <img src={product.image}/>
-        </li>
-      );
-    });
   }
 
   render() {
